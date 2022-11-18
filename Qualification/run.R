d1ab4636178ade67c88b2126fe0edb338f575f93
Michael Sevestre <michael@design2code.ca>
2022-11-18 15:45:10 -0500

    Initial version (#1)
    * Initial version
    
    * Ready for review

---
+++ Qualification/run.R
@@ -0,0 +1,8 @@
+rm(list = ls())
+source("C:/dev/Qualification-CKD/Qualification/workflow.R")
+
+createQualificationReport(
+   qualificationRunnerFolder="C:/dev/QualificationRunner/src/QualificationRunner/bin/Debug/net472",
+   pkSimPortableFolder= "C:/dev/Releases/v11/PK-Sim_11.0.150",
+   versionInfo = QualificationVersionInfo$new("1.0", "11.0", "2.0")
+   )
