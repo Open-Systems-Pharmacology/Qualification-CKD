a6f8bf4e3206376560df26440bdbc2f1f15acbe1
Michael Sevestre <michael@design2code.ca>
2022-11-23 11:26:29 -0500

    Fixes #6

--- Qualification/run.R
+++ Qualification/run.R
@@ -4,5 +4,5 @@ source("C:/dev/Qualification-CKD/Qualification/workflow.R")
 createQualificationReport(
    qualificationRunnerFolder="C:/dev/QualificationRunner/src/QualificationRunner/bin/Debug/net472",
    pkSimPortableFolder= "C:/dev/Releases/v11/PK-Sim_11.0.150",
-   versionInfo = QualificationVersionInfo$new("1.0", "11.0", "2.0")
+   versionInfo = QualificationVersionInfo$new("1.0", "11.0", "3.0")
    )
